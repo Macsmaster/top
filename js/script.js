d5af018b49b9909dc8a9fd108ec424c0b21d98fe
Miguel Coy <miguelcoytriana@gmail.com>
2019-07-21 17:03:16 -0500

    top

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var btnMenu = document.getElementById('icon-menu');
+var boton = document.getElementById('boton');
+
+btnMenu.addEventListener('click', function(){
+boton.classList.toggle('mostrar');
+})
+
+
